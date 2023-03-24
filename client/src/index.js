a9c45f1876e7572aba12a886bbf744cf1bb46f6c
wojciech krupa <wojciech.w.krupa@gmail.com>
2023-03-24 20:42:41 +0000

    Redux setup completed

--- client/src/index.js
+++ client/src/index.js
@@ -7,12 +7,22 @@ import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import { theme } from "./theme";
 
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./state/index";
+
+const store = configureStore({
+  reducer: { cart: cartReducer },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
--- client/src/scenes/global/Navbar.jsx
+++ client/src/scenes/global/Navbar.jsx
@@ -1,4 +1,75 @@
+import { useDispatch, useSelector } from "react-redux";
+import { Badge, Box, IconButton } from "@mui/material";
+import {
+  PersonOutline,
+  ShoppingBagOutlined,
+  MenuOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { shades } from "../../theme";
+// import { setIsCartOpen } from "../../state";
+
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  return (
+    <nav className="">
+      <Box
+        className=""
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+        width="100%"
+        height="60px"
+        backgroundColor="rgba(0,255,255,0.8)"
+        position="fixed"
+        top="0"
+        left="0"
+        zIndex="1"
+      >
+        <Box
+          className=""
+          width="80%"
+          // margin="auto"
+          display="flex"
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Box
+            className=""
+            onClick={() => navigate("/")}
+            sx={{
+              "&:hover": { cursor: "pointer" },
+            }}
+            color={shades.secondary[600]}
+          >
+            RIDE RITE
+          </Box>
+          <Box
+            className=""
+            display="flex"
+            alignItems="center"
+            justifyContent="space-between"
+            columnGap="20px"
+            zIndex="2"
+          >
+            <IconButton sx={{ color: "black" }}>
+              <SearchOutlined />
+            </IconButton>
+            <IconButton sx={{ color: "black" }}>
+              <PersonOutline />
+            </IconButton>
+            <IconButton sx={{ color: "black" }}>
+              <ShoppingBagOutlined />
+            </IconButton>
+            <IconButton sx={{ color: "black" }}>
+              <MenuOutlined />
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
+    </nav>
+  );
 };
 export default Navbar;
